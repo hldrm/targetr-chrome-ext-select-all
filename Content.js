464c1e8ce322e1be06c69b1c4a87ca487b147291
Maximilian Hild <59829610+hldrm@users.noreply.github.com>
2024-08-26 18:35:06 +0200

    restructure code and remove unneeded comments

--- Content.js
+++ Content.js
@@ -45,43 +45,14 @@ const buttonClickListener = (selectOrDeselect) => {
     const inputs = td.getElementsByTagName('input');
     for (let ip of inputs) {
       //ip.click();
+      // we are directly setting "checked" instead of clicking, to
+      // ensure consistency (all checked / unchecked)
       ip.checked = selectOrDeselect;
-      // TODO HLDRM mit checked = true / false rechnen? dann könnte man 2
-      // buttons machen. einen um alle auszuwählen, anderen um alle abzuwählen?
-      // oder overkill?
     }
   }
-  /*tds.map(td => {
-    let ip = td.getElementsByTagName('input');//[0].click();
-    console.log(ip);
-    ip[0].click();
-    return ip[0];
-  });*/
-
-  /*tds.forEach((td) => {
-    //inputs.append(td.getElementsByTagName('input'));
-    td.getElementsByTagName('input').click();
-  });*/
-
-
-  /*let checkboxes = $('.contentPanel td input');
-
-  let number = checkboxes.length;* /
-
-  for (i = 0; i < number; i++) {
-      / *if (!checkboxes[i]) {
-          continue;
-      }* /
-
-      //checkboxes[i].click();
-
-      $('.contentPanel td input')[i].click();
-  }
-  */
 };
 
 const insertButton = (title, selectOrDeselect) => {
-  //let paginationElem = $('.contentPanel ul.pagination');
   const contentPanels = document.getElementsByClassName('contentPanel');
   if (!contentPanels || !contentPanels[0]) {
     return false;
@@ -99,25 +70,18 @@ const insertButton = (title, selectOrDeselect) => {
   const paginationElem = paginationElems[0];
   myLog(paginationElem);
 
-  //let button = new Node();
   const liElement = document.createElement('li');
   liElement.classList.add('enabled');
 
   const button = document.createElement('button');
   button.classList.add('btn', 'btn-default');
   button.textContent = title;
+  button.addEventListener('click', () => buttonClickListener(selectOrDeselect));
 
   const icon = document.createElement('i');
   icon.classList.add('fa', 'fa-check-square-o');
-
   button.insertBefore(icon, button.firstChild);
 
-/*  button.onClick = (e) => {
-    window.alert('clicked!');
-  };*/
-  button.addEventListener('click', () => buttonClickListener(selectOrDeselect));
-
-  //paginationElem.after(button);
   paginationElem.appendChild(liElement);
   liElement.appendChild(button);
 };
@@ -128,37 +92,3 @@ const insertButton = (title, selectOrDeselect) => {
   insertButton('Select All', true);
   insertButton('Deselect All', false);
 })();
-
-// HLDRM start: old solution with simple timeout. not needed now wtih promise.
-
-//setTimeout(() => {
-  /*let windowConsole = console;
-  let console = {};
-  window.console = console;
-  console.log = (text) => {
-    windowConsole.log('TargetR clickR: ' + text);
-  };*/
-
-  //insertButton();
-
-//}, 2000);
-// HLDM end
-
-/*let checkboxes = $('.contentPanel td input');
-
-let number = checkboxes.length;* /
-
-for (i = 0; i < number; i++) {
-    / *if (!checkboxes[i]) {
-        continue;
-    }* /
-
-    //checkboxes[i].click();
-
-    $('.contentPanel td input')[i].click();
-}
-*/
-
-// console.log(checkboxes);
-
-// for (i=0; i<5; i++) { $('.contentPanel td input')[i].click(); }
